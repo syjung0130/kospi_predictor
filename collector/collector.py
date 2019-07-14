a5a29d6bb5423e16fb0c71e98f1154aa92a009c8
SOONYOUNG JUNG <soonyoung87@gmail.com>
2019-07-14 23:49:31 +0900

    코드 수정

--- collector/collector.py
+++ collector/collector.py
@@ -145,39 +145,3 @@ hourly_collector = HourlyCollector("035420")
 hourly_collector.get_html_page()
 hourly_collector.update_price()
 
-# daily_collector = DailyCollector("035420")
-# daily_collector.read_stock_data()
-
-
-# # BeautifulSoup를 이용해서 가져온 html을 parsing, 필요한 정보를 구성
-# # BeautifulSoup으로 html소스를 python객체로 변환하기
-# # 첫 인자는 html소스코드, 두 번째 인자는 어떤 parser를 이용할지 명시.
-# # 이 글에서는 Python 내장 html.parser를 이용했다.
-# soup = BeautifulSoup(html, 'html.parser')
-# #recruit_info_list > ul > li:nth-child(1) > div > div > h2 > a
-# print('[soup] h1: {0}'.format(soup.h1))
-# print('[soup] h2: {0}'.format(soup.h2))
-
-
-# all_text = soup.find(id="recruit_info_list")
-# print('[soup] ({0}) all_text: {1}'.format(len(all_text), type(all_text)))
-# print('[soup] ul: {0}'.format(len(all_text.ul)))
-# print('[soup] ul.li: {0}'.format(len(all_text.ul.li)))
-
-    
-# li_list = all_text.ul.li
-# cnt = 0
-# for li in li_list:
-#     # print('[soup] li: ({0})'.format(li))
-#     # print('[soup] li.a: {0}'.format(li.find("a")))
-#     if(cnt == 1):
-#         print('=============================')
-#         # print('[soup] li type : {0}'.format(type(li)))
-#         # print('[soup] li.a type : {0}'.format(type(li.find("a"))))
-#         # print('[soup] li.a: {0}'.format(li.find("a")))
-#         print('[soup] li.a.href: {0}'.format((li.find("a")).get('href')))
-#         print('[soup] li.a.title: {0}'.format((li.find("a")).get('title')))
-#         print('[soup] li.a.span: {0}'.format((li.find("a")).find('span')))
-        
-#         print('=============================')
-#     cnt += 1
