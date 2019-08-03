25de98086ed1817da8625e40a5ca0da018ed7903
SOONYOUNG JUNG <soonyoung87@gmail.com>
2019-08-03 15:52:56 +0900

    daily dataset db 연동기능 구현(refactoring)

--- collector/kospi_db_manager.py
+++ collector/kospi_db_manager.py
@@ -12,9 +12,12 @@ class KospiDBManager:
     def __init__(self, code):
         self.str_code = code
         self.open_db()
+        self.day_interval = 1
 
     def open_db(self):
         self.connection = sqlite3.connect("./kospi.db")
+        self.pd_kospi_db = pd.read_sql("SELECT * FROM '{}'".format(self.str_code), self.get_connection(), index_col = 'Date')
+        self.pd_read_sql("SELECT * FROM '{0}'".format(self.str_code))
     
     def close_db(self):
         self.connection.close()
@@ -25,54 +28,54 @@ class KospiDBManager:
     def get_connection(self):
         return self.connection
 
+    def pd_read_sql(self, strQuery):
+        self.pd_df_kospi_db = pd.read_sql(strQuery, self.get_connection())
+
+    def pd_write_db(self):
+        self.pd_df_kospi_db.to_sql('{}_dataset'.format(self.str_code), self.get_connection())
+
+    def add_column(self, strColumn):
+        strQuery = "ALTER TABLE '{0}' ADD '{1}' INTEGER".format(self.str_code, strColumn)
+        self.connection.execute(strQuery)
+        self.apply_to_db()
+
     def add_labelled_data(self):
         print('===== add_labelled_data() =====')
+        self.pd_read_sql("SELECT * FROM '{0}'".format(self.str_code))
+        self.add_gradient()
         self.add_price_status()
-        self.add_invest_decision()
+        self.pd_write_db()
     
-    def add_price_status(self):
-        print('===== add_price_status() =====')
-    
-    def add_invest_decision(self):
-        print('===== add_invest_decision() =====')
-    
-    def check_kospi_db(self):
-        self.open_db()
-        connection = self.get_connection()
-        pd_kospi_db = pd.read_sql("SELECT * FROM '{}'".format(self.str_code), connection, index_col = 'Date')
-        print(pd_kospi_db.head)
-
-        # Add columns PriceStatus, Decision.
-        strQuery = "ALTER TABLE '{}' ADD Gradient INTEGER".format(self.str_code)
-        connection.execute(strQuery)
-        strQuery = "ALTER TABLE '{}' ADD PriceStatus INTEGER".format(self.str_code)
-        connection.execute(strQuery)
-        connection.commit()
-
-        # read whole db to pandas dataframe
-        pd_check_kospi_db = pd.read_sql("SELECT * FROM '{}'".format(self.str_code), connection, index_col = 'Date')
-        # print(pd_check_kospi_db.head)
-        # strQuery = "SELECT Close FROM '{0}' WHERE Date='2009-05-06 00:00:00'".format(self.str_code)
+    def add_gradient(self):
+        print('===== add_gradient() =====')
+        # Add Gradient column
+        self.add_column('Gradient')
 
         # Add Gradient data
         day_interval = 1
-        strQuery = "SELECT * FROM '{0}'".format(self.str_code)
-        pd_df_kospi_db = pd.read_sql(strQuery, connection)
-        pd_df_kospi_db['Gradient'] = np.gradient(pd_df_kospi_db['Close'].rolling(center=False, window=day_interval).mean())
 
-        # Add PriceStatus data
-        for i in range(len(pd_df_kospi_db.index)):
-            if(float(pd_df_kospi_db.loc[i, 'Gradient']) > 0):
-                pd_df_kospi_db.loc[i, 'PriceStatus'] = eGradientStatus.INCREASE
-            elif(float(pd_df_kospi_db.loc[i, 'Gradient']) < 0):
-                pd_df_kospi_db.loc[i, 'PriceStatus'] = eGradientStatus.DECREASE
+        # 변화율(기울기)계산값을 로우로 추가 (numpy.gradient)
+        self.pd_df_kospi_db['Gradient'] = np.gradient(self.pd_df_kospi_db['Close'].rolling(center=False, window=self.day_interval).mean())
+    
+    def add_price_status(self):
+        print('===== add_price_status() =====')
+        # Add PriceStatus column
+        self.add_column('PriceStatus')
+
+        # Add PriceStatus data(상향/하향/변곡점)
+        for i in range(len(self.pd_df_kospi_db.index)):
+            if(float(self.pd_df_kospi_db.loc[i, 'Gradient']) > 0):
+                self.pd_df_kospi_db.loc[i, 'PriceStatus'] = eGradientStatus.INCREASE
+            elif(float(self.pd_df_kospi_db.loc[i, 'Gradient']) < 0):
+                self.pd_df_kospi_db.loc[i, 'PriceStatus'] = eGradientStatus.DECREASE
             else:
-                pd_df_kospi_db.loc[i, 'PriceStatus'] = eGradientStatus.INFLECTION
-        
-        pd_df_kospi_db.to_sql('kospi_predict.db', connection)
+                self.pd_df_kospi_db.loc[i, 'PriceStatus'] = eGradientStatus.INFLECTION
+    
+    def check_kospi_db(self):
+        self.open_db()
+        self.add_labelled_data()
 
-
+# kospi_db_manager 검증용 코드
 str_code = "035420"
 db_manager = KospiDBManager(str_code)
-db_manager.add_price_status()
 db_manager.check_kospi_db()
